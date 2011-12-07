e96f6f016814147e182860dd316cda5dd4ec14d1
uo.stratics <uo.stratics@gmail.com>
2011-12-07 18:25:28 +0000

    Added the MULTIPLY, SCREEN, OVERLAY, HARD LIGHT, SOFT LIGHT, DODGE and BURN blends.

--- pyprocessing/constants.py
+++ pyprocessing/constants.py
@@ -5,7 +5,7 @@ import pyglet.gl as gl
 #  CONSTANTS
 #************************
 
-version = "0.1.3.8" # This version of pyprocessing
+version = "0.1.3.9" # This version of pyprocessing
 
 #keycodes
 F1 = 65470
